b1017c229abed8e8305828924f545be95fe6daf8
karnika gupta <karnikagupta1830@gmail.com>
2022-10-06 17:10:35 +0530

    Button fixed

--- src/components/buttons/CustomBtn/styles.ts
+++ src/components/buttons/CustomBtn/styles.ts
@@ -4,7 +4,8 @@ const styles: {
   [key: string]: SxProps;
 } = {
   btn: {
-    width: "100%",
+    width: "500px",
+    marginLeft: "670px",
     borderRadius: "20px",
     backgroundColor: "#8A2BE2",
   },
